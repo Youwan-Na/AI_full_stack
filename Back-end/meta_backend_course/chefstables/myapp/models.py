c6cbce851c7d10332f09ab3c00f5cdb6012a8924
youwan <sktprp@gmail.com>
2024-10-21 20:50:55 +0900

    DB Study

--- Back-end/meta_backend_course/chefstables/myapp/models.py
+++ Back-end/meta_backend_course/chefstables/myapp/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.Model):
-    name = models.CharField(max_length= 100)
-    cuisine = models.CharField(max_length=100)
-    price = models.IntegerField()
-
-    def __str__(self):
-        return self.name + " : " + self.cuisine
-
+class Menuitems(models.Model):
+    name = models.CharField(max_length= 200)
+    category = models.CharField(max_length=300)
+    year = models.IntegerField()
     
