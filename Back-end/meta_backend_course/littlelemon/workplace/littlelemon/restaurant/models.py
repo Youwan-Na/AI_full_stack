a83eb2e8872f117da15dd23df97a8dae8b6eb713
youwan <sktprp@gmail.com>
2024-11-05 21:21:03 +0900

    littlelemon

--- Back-end/meta_backend_course/littlelemon/workplace/littlelemon/restaurant/models.py
+++ Back-end/meta_backend_course/littlelemon/workplace/littlelemon/restaurant/models.py
@@ -11,5 +11,12 @@ class Booking(models.Model):
    def __str__(self):
       return self.first_name + ' ' + self.last_name
 
-
 # Add code to create Menu model
+class Menu(models.Model):
+   name = models.CharField(max_length=200)
+   price = models.IntegerField()
+   menu_item_description = models.TextField(max_length=1000, default='')
+   
+   def __str__(self):
+      return self.name
+   
